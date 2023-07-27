dc30c411f0320008fb017e977b229f126abb98f5
YangTe127 <103026151+YangTe127@users.noreply.github.com>
2023-07-27 14:51:06 +0800

    Add files via upload

---
+++ exceptions_to_complete.py
@@ -0,0 +1,13 @@
+"""
+CP1404 - Practical
+exceptions to complete
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Enter a valid integer: "))
+        is_finished = True
+    except ValueError:
+        print("Please enter a valid integer.")
+print("Valid result is:", result)
